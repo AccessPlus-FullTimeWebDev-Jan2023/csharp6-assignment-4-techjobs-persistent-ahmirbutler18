a126dc1993c9741bbab7b95deaebf14531fef5c9
Ahmir <ahmirbutler18@gmail.com>
2023-04-22 21:44:56 -0500

    completed assignment 4

--- TechJobs6Persistent/Controllers/EmployerController.cs
+++ TechJobs6Persistent/Controllers/EmployerController.cs
@@ -13,31 +13,54 @@ using TechJobs6Persistent.ViewModels;
 namespace TechJobs6Persistent.Controllers
 {
     public class EmployerController : Controller
-    { 
+    {
+
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+            return View(employers);
         }
 
         [HttpGet]
         public IActionResult Create()
+
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+            return View(addEmployerViewModel);
         }
 
         [HttpPost]
-        public IActionResult ProcessCreateEmployerForm()
+        public IActionResult ProcessCreateEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
-        }
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
 
-        public IActionResult About(int id)
+                return Redirect("/Employer");
+            }
+            return View("Create", addEmployerViewModel);
+        }
+     public IActionResult About(int Id)
         {
-            return View();
-        }
+            // search through the Employer table inside JobDbContext and find the Id
+            Employer newEmployer = context.Employers.Find(Id);
 
+            return View(newEmployer);
+        }
     }
 }
-
--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -4,7 +4,25 @@
 
 --Part 1: List the columns and their data types in the Jobs table.
 
+--Job table contains
+--Id (INT)
+--Name(longtext)
+--EmployerId(INT)
+
+
+
 --Part 2: Write a query to list the names of the employers in St. Louis City.
+
+--select *
+--from employers
+--where location = 'st. louis';
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+
+ --select skillname
+ --from skills
+ --right join jobskills 
+ --on jobskills.skillsid = skills.id
+ --where skillsid is not null
+ --order by skillname asc
